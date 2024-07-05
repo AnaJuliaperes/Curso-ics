c221e56e6683bc7322e378472257b460a7c83d20
AnaJuliaperes <162056822+AnaJuliaperes@users.noreply.github.com>
2024-07-05 17:49:44 -0300

    Refeitos finalizado

---
+++ refeitos/ex10.py
@@ -0,0 +1,5 @@
+def soma(valor1, valor2):
+    return valor1 + valor2
+
+resultado = soma(50,684)
+print(resultado)
---
+++ refeitos/ex16.py
@@ -0,0 +1,4 @@
+def calcula_media():
+    contador = 0
+    tota = 0
+    while contador 
