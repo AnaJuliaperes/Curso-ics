1bed6d17cb97857e3dac0ba23c7cb0bcc39aacae
ana julia <anajuliaquipe09@gmail.com>
2024-10-16 16:57:44 -0300

    teste3

--- Refeitos/ex10.py
+++
@@ -1,5 +0,0 @@
-def soma(valor1, valor2):
-    return valor1 + valor2
-
-resultado = soma(60,212)
-print(resultado)
--- refeitos/ex10.py
+++
@@ -1,5 +0,0 @@
-def soma(valor1, valor2):
-    return valor1 + valor2
-
-resultado = soma(50,684)
-print(resultado)
