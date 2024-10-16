616848e7f4dc30d47ad2f34f9e85684b725dc925
ana julia <anajuliaquipe09@gmail.com>
2024-10-16 16:52:41 -0300

    teste

--- refeitos/ex16.py
+++ refeitos/ex16.py
@@ -1,4 +1,4 @@
-def calcula_media():
-    contador = 0
-    tota = 0
-    while contador 
+def valores (valor1, valor2, valor3, valor4):
+    
+    total = valores(47, 44, 15, 14) /4
+    print(total)
